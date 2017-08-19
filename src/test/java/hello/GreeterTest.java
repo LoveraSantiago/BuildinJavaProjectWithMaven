162d0448b8c2c0d461a9a33da9f77d174d990d30
Santiago Lovera <santiago.lovera@gmail.com>
2017-08-18 22:40:55 -0300

    Exemplo relembrar um java maven project

---
+++ src/test/java/hello/GreeterTest.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GreeterTest {
+	
+	private Greeter greeter = new Greeter();
+	
+	@Test
+	public void greeterSayHello(){
+		Assert.assertThat(greeter.sayHello(), CoreMatchers.containsString("Hello"));
+	}
+
+}
